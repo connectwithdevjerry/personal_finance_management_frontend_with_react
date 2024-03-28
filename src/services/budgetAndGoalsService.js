d7bceb5574fcb69cac3aac29c54764e9074debc5
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-03-28 10:25:28 +0100

    just commit - Finance-management-app

--- src/services/budgetAndGoalsService.js
+++ src/services/budgetAndGoalsService.js
@@ -1,15 +1,15 @@
-// // services/budgetAndGoalsService.js
+// services/budgetAndGoalsService.js
 
-// import axios from 'axios';
+import axios from 'axios';
 
-// const saveBudgetAndGoals = async (budgetGoalData) => {
-//   try {
-//     const response = await axios.post('http://localhost:5000/api/budgetAndGoals/save', budgetGoalData);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error saving budget and goals:', error);
-//     throw error;
-//   }
-// };
+const saveBudgetAndGoals = async (budgetGoalData) => {
+  try {
+    const response = await axios.post('http://localhost:5000/api/budgetAndGoals/save', budgetGoalData);
+    return response.data;
+  } catch (error) {
+    console.error('Error saving budget and goals:', error);
+    throw error;
+  }
+};
 
-// export { saveBudgetAndGoals };
+export { saveBudgetAndGoals };
